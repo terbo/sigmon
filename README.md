0725d4c671b874e8d5764ca800c82a3f248b015d
terbo <terry.chad@gmail.com>
2014-10-17 23:23:34 -0700

    Fixed tail/csv and debug display Prettier code Readme has more info

--- README.md
+++ README.md
@@ -1,82 +1,99 @@
-sigmon
+sigmon 0.9c
 ======
 
-Display wireless probe requests like airodump-ng in python and scapy
+Display wireless probe requests like airodump-ng in python and scapy<br>
 
-This program displays wireless probe requests from associated and unassociated wireless clients in range.
-The output is similar to that of airodump-ng from the aircrack-ng suit.
+This program displays wireless probe requests from associated and unassociated wireless clients in range.<br>
+The output is similar to that of airodump-ng from the aircrack-ng suit.<br>
 
-It will also play a sound when a new client is detected (the kismet sound, available @ http://goo.gl/oDi5sR) and can
-be instructed to note familiar devices (favorites) such as machines you own.
+It will also play a sound when a new client is detected (the kismet sound, available @ http://goo.gl/oDi5sR) and can<br>
+be instructed to note familiar devices (favorites) such as machines you own.<br>
 
-Requirements
+The code is released under the GPL2 license.
+
+What it does
 ============
 
-You will need a wireless card that is capable of going into <a href=http://en.wikipedia.org/wiki/Monitor_mode>monitor mode</a>, and is compatible with <a href=http://www.aircrack-ng.org/>aircrack</a>.
+Nowadays radio waves are crowded with signals; indeed, nearly every person has a radio transmitter
+on them; some people have several. These transmitters are personal cellular, wireless, and bluetooth
+devices, and this program focuses on listening to the wireless signals.
 
-Only tested it on one machine! But it should work on others.
+Each time you connect to a wireless network and your device remembers the name, it indefinitally searches
+for it, as long as wireless is turned on. The way it does this is by sending out 'probe' requests that
+contain the name of the access point you are looking for, in the form of a service set identifer (<a href=http://en.wikipedia.org/wiki/SSID)>SSID</a>).
+Also contained in this probe is your machine access control (<a href=http://en.wikipedia.org/wiki/MAC_Address>MAC</a>) address, which is completely unique to
+your device.
 
-Also required are of course <b>python</b> and the <b>scapy</b> libraries, and also <b>netaddr</b> for mac address lookups.
+These probes are sent into the airwaves unencrypted. What this means is that anyone listening for these
+probes can (<b>a</b>) uniquely identify each device and (<b>b</b>) view what networks each device is looking for.
+This program displays that information.
+
+Also included in this information is a relative signal strength, which can be used to determine approximate
+distance from the base station; with more base stations, finer granularity in location can be achieved.
+
+While some percentage of people have smart phones, a smaller number have wireless always enabled.
+Even so, the number of results this program shows can be used to form a demographic map.
+
+Other uses for this data can be discovered; at the end of this file videos are linked that include some
+of the wide ranging security implications.
+
+The Requirements
+============
+
+You will need a wireless card that is capable of going into <a href=http://en.wikipedia.org/wiki/Monitor_mode>monitor mode</a>, and is compatible with <a href=http://www.aircrack-ng.org/>aircrack</a>.<br>
+
+Only tested it on one machine, but it should work on others.<br>
+
+Also required are of course <b>python</b> and the <b>scapy</b> libraries.<br>
+Also, <b>netaddr</b> for mac address lookups, and <b>humanize</b> for pretty numbers.<br>
 
 The testing platform is <b>Kali Linux</b> 1.0.9 running on an i686 kernel version 3.14.<br>
-The tested chipset was a realtek 8187.
+The tested chipsets were a realtek 8187 and an atheros ar9271.<br>
 
 Usage
 =====
 
-Running sigmon.py without any arguments will begin listening for probes from the default mon0 interface.
-airmon-ng must be run prior to create the monitor interface from the wireless interface. Use -i to choose
-another interface; only one is currently supported.
+Running sigmon.py without any arguments will begin listening for probes from the default mon0 interface. <br>
+airmon-ng must be run prior to create the monitor interface from the wireless interface. <br>
+Use -i to choose another interface; only one is currently supported. <br>
 
 Use the -f flag to favorite all of your own devices, and note them in the display.<br>
-E.g. sigmon.py -f 00:00:00:00:00:00
+E.g. sigmon.py -f 00:00:00:00:00:00 -f 00:00:00:00:00:01 <br>
 
-A tailable version will come soon; this will be vaguely different from a tshark one-liner.
+Use sigmon.py -t to output a tailable csv version (all information on one line)
+
+Use sigmon.py -l [number] to listen for a limited number of packets, then exit.
+This is not the same as the number of well-formed wireless packets it will display.
 
 <pre>
-	-d		print debug to stdout (--debug)
-	-f		add a mac to favorite list (--fav)
-	-i		select interface (--interface)
+sigmon [interface] 
+	-h		show this help 
+
+	-f		add a mac to favorite list (--fav) 
+	-i		select interface (--interface) 
+	-l		stop after x number of packets (--limit) 
+	-t		tailable output (--tail) 
+	-d		print debug to stdout (--debug) 
+
+version 0.9c 
 </pre>
 
 Examples
 ========
 <pre>
-root#kali pts/0-/root/code/sigmon] ./sigmon --print
-
- mon0 PKTS: 2297 [ Elapsed: 65 ][ 1413501263 ][ 8 Clients ][ 2 SSIDs ][ sorting by signal level
-
-				Close Clients:
-
- STATION				PWR	Frames	Probes
-
-				Farther clients:
-
- STATION				PWR	Frames	Probes
-
- 34:23:ba:xx:xx:xx (Samsung E)		-75	2	[ANY]
- 1c:99:4c:xx:xx:xx (Murata Ma)		-74	2	[ANY]
- 30:19:66:xx:xx:xx (Samsung E)		-74	17	[ANY],linksys
- cc:9e:00:xx:xx:xx (Nintendo )		-75	2	NETGEAR
- 00:ee:bd:xx:xx:xx (HTC Corpo)		-71	5	[ANY]
- 34:4d:f7:xx:xx:xx (LG Electr)		-73	7	[ANY]
- 1c:99:4c:xx:xx:xx (Murata Ma)		-75	1	[ANY]
- 40:0e:85:xx:xx:xx (Samsung E)		-78	1	[ANY]
-</pre>
-
+root#kali pts/0-/root/code/sigmon] ./sigmon
 
 Caveats
 =======
 
-Some wireless cards won't report correct signal levels<br>
-Currently doesn't save any information<br>
+Some wireless cards won't report correct signal levels (some onboard laptop cards)<br>
+Currently doesn't save any information (looking into couchdb)<br>
 Will fill your screen after 10000 packets or a couple dozen clients<br>
-Can cause your cat to do weird things<br>
+Can cause your cat to do weird things<br> 
 
 TODO
 ====
 
-Add pretty printed numbers<br>
 Add other output options, couchdb, sqlite, sqlachemy<br>
 Add curses display :D<br>
 
@@ -95,7 +112,7 @@ CreepyDOL - blog.ussjoin.com/2013/08/creepydol.html and https://github.com/ussjo
 
 Videos:
 
-https://www.youtube.com/watch?v=GvrB6S_O0BE The Machines That Betrayed Their Masters by Glenn Wilkinson_<br>
+https://www.youtube.com/watch?v=GvrB6S_O0BE - The Machines That Betrayed Their Masters by Glenn Wilkinson_<br>
 https://www.youtube.com/watch?v=ubjuWqUE9wQ - DEFCON 21 - Stalking a City for Fun and Frivolity<br>
 https://www.youtube.com/watch?v=NjuhdKUH6U4 - DEFCON 20 - Can You Track Me Now? Government And Corporate Surveillance Of Mobile Geo-Location<br>
 
--- sigmon.py
+++ sigmon.py
@@ -1,29 +1,35 @@
 #!/usr/bin/python -u
-#
-# probe.py v.09a - cbt 10/01/14
 
 '''
+probe.py v.09c - cbt 10/01/14
+last modified 17 oct 01:48 pst
+
 taken from somewheres else ....
 listen for wireless probe requests
 
 TODO
-Need a pretty print for uptime (number_format)
-and for the date
+Add CouchDB (easy) or sqlalchemy (coding curve)
+
+Work out various displays, 'noisy' clients, common ssids
+
+-- add couchdb -> add redisplay/statistics/graphs
+-- add threading -> add multiple interfaces -> add ncurses
 
 '''
 
-import os, sys, time, getopt # sched, json
-import logging
-logging.getLogger('scapy.runtime').setLevel(logging.ERROR)
+import os, sys, signal, string, datetime as dt, time # sched, json
+import logging, getopt, humanize as pp
+
+logging.getLogger('scapy.runtime').setLevel(logging.ERROR) # quiet scapy ipv6 error
 
 from scapy.all import *   # the almighty scapies - packet crafting and manipulation
 from netaddr import *     # MAC Address OUI Vendor Lookups - could offload to own db
-from commands import *
-from subprocess import *
 
-# packet specification: used to hold packet information before being placed into conf.client
+# packet specification
+# used to hold packet information before being placed into conf.client
 class Packet():
   lastseen=None
+  firstseen=None
   mac=None
   ssid=None
   crypto=set()
@@ -33,7 +39,10 @@ class Packet():
   gps=None
   channel=None
   packetype=None # bluetooth, wifi, nfc, cell
+  ptype=0
+  psubtype=0
   packets=0
+  size=0
 
 # client observation
 class Client:
@@ -43,7 +52,7 @@ class Client:
   probes=0
   ssids=set()
   bssid=None
-  signal=0
+  signal=list()
   channel=0
   vendor=None
   
@@ -55,7 +64,7 @@ class Client:
     self.lastseen=None
     self.probes=0
     self.bssid=None
-    self.signal=0
+    self.signal=list()
     self.vendor=None
 
 # favorite mac addresses
@@ -69,36 +78,56 @@ class Fav:
 
 # the configuration object, with everything in it (including globals and defaults options)
 class CONF(object):
-  version = '0.9a'
-  c = dict() # clients
-  opts = set() 
+  version = '0.9c'
+  interface = 'mon0'
+  
   limit = 0
   packets = 0
+  probes = 0
   uptime = 0
+  
+  # signal threshold for nearby devices
   signal_max = -60
+  
   clientcount = 0
   ssidcount = 0
+  
   fav = set()
+  
   sndplayer = '/usr/bin/play'
-  sndplayeropts = '-q'
-  newsound = 'new.wav'
-  interface = ''
+  sndplayeropts = '-V0'
+  newsound = '/root/code/sigmon/new.wav'
+  
+  c = dict() # clients
+  opts = set() 
+
+  interrupts = 0
+  interruptsmax = 5
+
 conf = CONF()
 
-# debug?
+def sigint(signal, frame):
+  debug('conf.opts: ' + str(list(conf.opts)))
+  debug('Probes: ' + pp.intcomma(conf.probes))
+  debug('Packets: ' + pp.intcomma(conf.packets))
+  debug('Signal Maximum: ' + str(conf.signal_max))
+  debug('Clients seen: ' + pp.intcomma(conf.clientcount))
+  debug('SSIDs seen: ' + pp.intcomma(conf.ssidcount))
+  debug('Favorites list: ' + str(list(conf.fav)))
+  debug('Interface: ' + conf.interface)
+  debug('Start time: ' + str(conf.uptime))
+
+  print('\nInterrupt received -- exiting.')
+  sys.exit(0)
+
+signal.signal(signal.SIGINT, sigint)
+
+# what bugs?
 def debug(log):
   if 'debug' not in conf.opts: return
-  '''
-  try:
-    debugoutput
-  except:
-    debugoutput = ''
-
-  debugoutput += log + '\n'
-  '''
-  print log
+  print "%s  %s" % (dt.datetime.now(), log)
   
-# remove all symbols, spaces, lowercase
+# TODO: remove all symbols, spaces, lowercase
 def macreg(mac):
   return
 
@@ -110,11 +139,23 @@ def clear_screen():
 
 # ansi print screen - simulate airodump-ng
 def show(out):
-  clear_screen()
-  print out
-  print '\n'
-#  print debugoutput
+  if 'debug' not in conf.opts and 'tail' not in conf.opts: clear_screen()
+  sys.stdout.write('%s\n' % out)
+  sys.stdout.flush()
 
+def usage():
+  print os.path.basename(__file__) + ' [interface]' + '\tlisten for wireless probe requests\n'
+  print '\t' + '-h' + '\t\t' + 'show this help'
+  print ''
+  print '\t' + '-f' + '\t\t' + 'add a mac to favorite list (--fav)'
+  print '\t' + '-i' + '\t\t' + 'select interface (--interface)'
+  print '\t' + '-l' + '\t\t' + 'stop after x number of packets (--limit)'
+  print '\t' + '-d' + '\t\t' + 'print debug to stdout (--debug)'
+  print '\t' + '-t' + '\t\t' + 'tailable (CSV) output (--tail)'
+  print ''
+  print 'version ' + conf.version
+  
+  sys.exit(1)
 
 # main packet processing function - called by scapy.sniff()
 # somehow I can't get threads/timers to work in conjunction with scapy
@@ -122,50 +163,65 @@ def show(out):
 # or read from a db in a seperate program
 
 def sniffpkts(packet):
-  #if('limit' in conf.opts and conf.packets >= conf.limit): sys.exit(0)
-  
   p = Packet()
   
-  # eliminate malformed/bad packets
-  try: packet
-  except:return debug('No Packet')
-  try: packet[Dot11].addr3
-  except: debug('No BSSID Address')
-  try: packet.addr2
-  except: debug('No MAC Address')
+  conf.packets += 1
   
-  #debug(packet.sprintf("Packet size: %IP.len%"))
-  #debug(packet[0].show())
+  if conf.limit != 0 and conf.probes >= conf.limit:
+    debug('Saw %s probes, exiting.' % (conf.limit))
+    sys.exit(0)
+  
+  #debug('Got ' + str(len(str(packet.payload))))
+  # trying to determine packet size?
+  
+  # eliminate malformed/bad packets
+  
+  try: packet
+  except:return #debug('No Packet')
+  
+  try: packet.info
+  except: return #debug('No Packet')
+  
+  try: packet.addr2
+  except: return #debug('No MAC Address')
+  
+  try: p.mac = packet.addr2 # check the mac later, maybe a mac class that returns xyz error
+  except: return #debug('No MAC Address')
+  
+  try: packet[Dot11].addr3
+  except: return #debug('No BSSID Address')
+  
+  try: p.bssid = packet[Dot11].addr3
+  except: return #debug('No BSSID Address')
+
+  p.lastseen = str(packet.time)
+  p.size = packet.sprintf('%IP.len%')
   
   try:
-    p.bssid = packet[Dot11].addr3
-    p.lastseen = str(packet.time)
-    p.mac = packet.addr2
-    p.signal = str(-(256-ord(packet.notdecoded[-4:-3])))
-  except: 
-    debug('Cant get SSID/Address/Signal ..')
+    p.ptype = packet.type
+    p.subtype = packet.subtype
+  except:
     return
- 
-  if(p.bssid == None or p.mac == None or p.signal == 0):
-   return
-
+  
+  p.signal = str(-(256-ord(packet.notdecoded[-4:-3])))
   
   try:
     EUI(p.mac)
     mac = EUI(p.mac)
     oui = mac.oui
     p.vendor = oui.registration().org
+  except TypeError:
+    debug('ERROR Resolving MAC: %s ' % (p.mac))
+    next
   except NotRegisteredError:
     p.vendor = 'UNREGISTERED'       # hackers change their macs ...
-  except TypeError:
-    debug('ERROR Resolving MAC: ' + str(p.mac))
-    return
 
   # if packet is a management/probe request
-  if (packet.haslayer(Dot11) and packet.type == 0 and packet.subtype in (0, 2, 4)) and 'probe' in conf.opts:
-    p.ssid = packet[Dot11Elt].info if len(packet.info) else '<ANY>'
+  if (packet.haslayer(Dot11) and packet.type == 0 and packet.subtype in (0,2,4)):
+    p.ssid = packet[Dot11Elt].info[:32] if len(packet.info) else '<ANY>'
     try: p.channel = ord(packet[Dot11Elt:3].info)
     except: p.channel = '0' # broadcast/any requests have no channel
+    
     p.packetype = 'probe'
   
     # last filter for bad packets
@@ -173,112 +229,128 @@ def sniffpkts(packet):
 
     try:
       conf.c[p.mac]
-      conf.c[p.mac].firstseen = p.lastseen
     except:
       debug('New Client: ' + p.mac)
-      subprocess.Popen([conf.sndplayer, conf.sndplayeropts, conf.newsound])
+      #subprocess.Popen([conf.sndplayer, conf.sndplayeropts, conf.newsound])
     
       conf.clientcount += 1
       
       conf.c[p.mac] = Client(p.mac)
-      conf.c[p.mac].lastseen = p.lastseen
+      conf.c[p.mac].firstseen = p.lastseen
       conf.c[p.mac].vendor = p.vendor
     
-
     conf.c[p.mac].bssid = p.bssid
-    conf.c[p.mac].signal = p.signal # TODO: make a list to show signal overtime
+    conf.c[p.mac].signal.append(p.signal)
+    conf.c[p.mac].lastseen = p.lastseen
+    p.firstseen = conf.c[p.mac].firstseen
     
     # increment probe count
     conf.c[p.mac].probes += 1
     
     if(p.ssid not in conf.c[p.mac].ssids):
-      debug('New SSID for Client ' + p.mac + '(' + p.vendor + '): ' + p.ssid)
+      debug('New SSID for Client %s (%s): %s' % (p.mac, p.vendor, p.ssid))
       # increment SSID count
       if p.ssid != '<ANY>': conf.ssidcount += 1
       conf.c[p.mac].ssids.add(p.ssid)
   
-  
-  # increment packet count
-  conf.packets += 1
+  else:
+     # some other type of packet
+     return
+
+  # increment (relevant) packet count
+  conf.probes += 1
 
   # re-add couchdb support later
   if 'print' in conf.opts:
-
-    output = '\n ' + conf.interface + ' PKTS: ' + str(conf.packets) + ' [ Elapsed: ' + str(int((time.time() - conf.uptime))) + \
-        ' ][ ' + str(int(time.time())) + ' ][ ' + str(conf.clientcount) + ' Clients ][ ' + str(conf.ssidcount) + ' SSIDs ][ sorting by signal level\n'
+    output = '\n IF: %s %s probes [ Started: %s ][ %s ][ %s Clients ][ %s SSIDs ][ sorting by %s' % \
+      ( conf.interface , pp.intcomma(conf.probes), \
+        pp.naturaltime( time.time() - conf.uptime ), dt.datetime.today(), \
+        pp.intcomma( conf.clientcount ), pp.intcomma( conf.ssidcount ), 'last seen')
     
-    header = '\n STATION\t\t\t\tPWR\tFrames\tProbes\n\n'
+    header = '\n STATION\t\t\t\t\tPWR\tProbes\n\n'
     
-    outputnear = '\n\t\t\t\tClose Clients:\n' + header;
-    outputfar = '\t\t\t\tFarther clients:\n' + header
+    outputnear = '\n\n\t\t\t\tClose Clients:' + header;
+    outputfar = '\t\t\t\tFarther clients:' + header
     
-    # list clients, sorted by signal - soon take key input and offer options . . . . and sort by close and far
+    # list clients, sorted by last seen - soon take key input and offer options . . . . and sort by close and far
     for client in sorted(conf.c, cmp=lambda a,b : cmp(conf.c[b].lastseen, conf.c[a].lastseen)):
       
-      debug('Outputting data for ' + str(client) + ' (' + str(conf.c[client].vendor) + ') ' + conf.c[client].signal + \
-          ' [' + str(conf.c[client].probes))
+      if 'tail' not in conf.opts:
+        debug('Outputting data for %s (%s) %sdBm [%s]' \
+        % (client, conf.c[client].vendor, \
+          conf.c[client].signal[-1], conf.c[client].probes))
+      
       # output list of clients and ssids
       
-      out = ' '
+      if(client in conf.fav): out = ' *'
+      else: out = '  '
+
+      # easy way of only printing 'valid' ssids from the set
       
-      if(client in conf.fav): out = '*'
+      ssids = ''.join(filter(lambda x:x in string.printable, string.join(list(conf.c[client].ssids),',')))
       
-      out += str(client) + ' (' + str(conf.c[client].vendor[:9]) + ')\t\t' + \
-        str(conf.c[client].signal) + '\t' + \
-        str(conf.c[client].probes) + '\t' + ','.join(conf.c[client].ssids) + '\n'
-      
+      out += '%-18s (%-18s)\t%-8s %-8s %s\n' % (client, \
+          conf.c[client].vendor[:18], conf.c[client].signal[-1], \
+          conf.c[client].probes, ssids)
+     
       # nearby signal
-      if(int(conf.c[client].signal) > conf.signal_max) :
+      if(int(conf.c[client].signal[-1]) > conf.signal_max) :
         outputnear += out
-      # far away signal
       else:
+      # far away signal
         outputfar += out
 
-    outputnear += '\n\n\n\n'
-    
-    if 'debug' not in conf.opts: show(output + outputnear + outputfar)
+      show(output + outputnear + '\n' + outputfar)
+  
+  elif 'tail' in conf.opts:
+    show("'%s','%s','%s','%s','%s','%s','%s'" % \
+        (p.mac, p.bssid, p.ssid, p.signal, \
+         p.firstseen, p.lastseen, p.vendor))
 
 def main(argv):
   conf.uptime = time.time()
   
-  conf.opts.add('probe')
-  conf.opts.add('print')
-
-  getopts = 'hi:df:'
-  getoptslong = ['help', 'interface=', 'debug', 'fav=']
+  getopts = 'hi:df:l:t'
+  getoptslong = ['help', 'interface=', 'debug', 'fav=', 'count=','tail']
   
   try:
     opts, args = getopt.getopt(argv, getopts, getoptslong)
   except:
-    print 'err'
-    sys.exit(2)
+    print 'Argument error.'
+    usage()
 
   for opt, arg in opts:
     if opt in ('-h', '--help'):
-      print os.path.basename(__file__) + ' [interface]'
-      print '\t' + '-h' + '\t\t' + 'show this help'
-      print ''
-      print '\t' + '-f' + '\t\t' + 'add a mac to favorite list (--fav)'
-      print '\t' + '-i' + '\t\t' + 'select interface (--interface)'
-      print '\t' + '-d' + '\t\t' + 'print debug to stdout (--debug)'
-      print ''
-      print 'version ' + conf.version
-      sys.exit()
-    
-    elif opt in ('-f', '--fav'):
-      debug('adding ' + arg + ' to favorites')
-      conf.fav.add(arg)
+      usage()
     
     elif opt in ('-d', '--debug'):
       conf.opts.add('debug')
     
+    elif opt in ('-l', '--limit'):
+      conf.limit = int(arg)
+    
+    elif opt in ('-f', '--fav'):
+      conf.fav.add(arg)
+      debug('added ' + arg + ' to favorites')
+    
+    elif opt in ('-t', '--tail'):
+      conf.opts.add('tail')
+    
     elif opt in ('-i', '--interface'):
       conf.interface = arg
 
-  if conf.interface == '': conf.interface = 'mon0' 
-  print 'Listening for packets from ' + conf.interface
+  if('debug' not in conf.opts and 'tail' not in conf.opts):
+    print 'adding print option'
+    conf.opts.add('print')
+  
+  print 'Listening for %s probes from %s ' % \
+    ( 'unlimited' if conf.limit == 0 else conf.limit, conf.interface )
 
-  sniff(iface=conf.interface, prn=sniffpkts)
+  if('tail' in conf.opts):
+    print 'mac,bssid,ssid,signal,firstseen,lastseen,vendor'
+  
+  # sniff count packets, and do not store them in memory
+  sniff(iface=conf.interface, prn=sniffpkts, store=0)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
