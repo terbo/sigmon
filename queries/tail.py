d9822c987f3500185d586d40b2a553b70357d796
terbo <terry.chad@gmail.com>
2014-10-18 23:41:13 -0700

    tail query works like original sigmon.py from couchdb

--- queries/tail.py
+++ queries/tail.py
@@ -1,20 +1,36 @@
 #!/usr/bin/python
 
 # tail the database
-# how do you just list new upate ... ^_o
 
-import pycouchdb as couchdb
+import os
+import sys
+import time
+import signal
 import datetime
+import pycouchdb as couchdb
 
 c = couchdb.Server()
-
 db = c.database('sigmon')
+uptime = datetime.datetime.now()
 
-def tail(msg, db):
+probes=0
+
+def sigint(signal, frame):
+  print('Seen %s probes since %s.' % ( probes, uptime ) )
+  print('Press Ctrl-\ to exit.')
+
+signal.signal(signal.SIGINT, sigint)
+
+def sigmon_tail(msg, db):
 	docid = msg['id']
+	
 	doc = db.get(docid)
-	print("'%s','%s','%s','%s','%s','%s','%s'" % \
-		(doc['mac'], doc['bssid'], doc['ssid'], doc['signal'], \
-		 doc['firstseen'], doc['lastseen'], doc['vendor']))
+	
+	global probes
+	probes += 1
 
-db.changes_feed(tail)
+	print("'%s','%s','%s','%s','%s','%s','%s'" % (doc['lastseen'], \
+			doc['firstseen'], doc['mac'], doc['bssid'], doc['ssid'], doc['signal'], doc['vendor']))
+	
+while True:
+	db.changes_feed(sigmon_tail, descending='true')
